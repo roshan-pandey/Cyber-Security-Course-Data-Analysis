d2e65cd1a3450b3f132bb5987753e9bdaead4a3f
roshan-pandey <roshanpandey330@gmail.com>
2021-11-15 15:28:43 +0000

    Analysis on which device is popular to access the course content

--- src/eda.R
+++ src/eda.R
@@ -1,8 +1,27 @@
 library('ProjectTemplate')
 load.project()
 
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
-}
+# for (dataset in project.info$data)
+# {
+#   message(paste('Showing top 5 rows of', dataset))
+#   print(head(get(dataset)))
+# }
+
+video_df = get(project.info$data[7])
+print(head(video_df))
+
+desktop_percentage = video_df %>% group_by(title) %>% summarize(total_desktop_views = mean(desktop_device_percentage))
+mobile_percentage = video_df %>% group_by(title) %>% summarize(total_mobile_views = mean(mobile_device_percentage))
+tablet_percentage = video_df %>% group_by(title) %>% summarize(total_tablet_views = mean(tablet_device_percentage))
+
+device_df = cbind(desktop_percentage, mobile_percentage[,2], tablet_percentage[,2])
+
+device_df = melt(device_df, id.vars = c('title'))
+
+png(file = 'C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/graphs/1.png', width = 1920, height = 1080)
+ggplot(device_df, aes(x = title, y = value, fill = variable))+
+  geom_bar(stat='identity', position = position_dodge())+
+  theme(axis.text.x = element_text(angle=90))
+dev.off()
+
+  
