0de0326813fbd711bf44272e13c120d24cde6b22
roshan-pandey <roshanpandey330@gmail.com>
2021-11-21 11:40:21 +0000

    analysis of watch time over different devices

--- src/eda.R
+++ src/eda.R
@@ -2,7 +2,7 @@ library('ProjectTemplate')
 load.project()
 
 # Selecting video stats data from all the iterations of the course...
-video_df = get(project.info$data[7])
+video_df = get(project.info$cache[7])
 
 # Finding which device is being use more often to access the course content...
 desktop_percentage = video_df %>% group_by(title) %>% summarize(desktop = mean(desktop_device_percentage))
@@ -14,8 +14,7 @@ device_df = cbind(desktop_percentage, mobile_percentage[,2], tablet_percentage[,
 
 # Converting the wide data to long data...
 device_df = melt(device_df, id.vars = c('title'))
-colnames(device_df)[2] <- "Device"
-colnames(device_df)[3] <- "Percentage_Watch"
+colnames(device_df) <- c('title', 'Device', 'Percentage_Watch')
 
 # saving the plot to the graphs/ directory...
 png(file = 'C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/graphs/1.png', width = 1920, height = 1080)
@@ -24,55 +23,38 @@ ggplot(device_df, aes(x = title, y = Percentage_Watch, fill = Device))+
   theme(axis.text.x = element_text(angle=90))
 dev.off()
 
+# Extracting all the files with video stats...
 vid_filenames <- list.files("C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/raw_data", pattern="*.csv", full.names=TRUE)
 vid_ldf <- lapply(vid_filenames, read.csv)
 vid_index = grep("video_duration", vid_ldf)
 
-vid_itr3 = vid_ldf[vid_index[1]][[1]][, c(2, 17, 18, 20)]
-vid_itr3 = melt(vid_itr3, id.vars = c('title'))
-colnames(vid_itr3)[2] <- "Device"
-colnames(vid_itr3)[3] <- "Percentage_Watch"
 
-vid_itr4 = vid_ldf[vid_index[2]][[1]][, c(2, 17, 18, 20)]
-vid_itr4 = melt(vid_itr4, id.vars = c('title'))
-colnames(vid_itr4)[2] <- "Device"
-colnames(vid_itr4)[3] <- "Percentage_Watch"
+# Calculating mean of watch percentage of each device for each iteration of the course...
+mean_vid_df3 = vid_ldf[vid_index[1]][[1]][, c(17, 18, 20)]
+col_means3 = colMeans(mean_vid_df3)
 
-vid_itr5 = vid_ldf[vid_index[3]][[1]][, c(2, 17, 18, 20)]
-vid_itr5 = melt(vid_itr5, id.vars = c('title'))
-colnames(vid_itr5)[2] <- "Device"
-colnames(vid_itr5)[3] <- "Percentage_Watch"
+mean_vid_df4 = vid_ldf[vid_index[2]][[1]][, c(17, 18, 20)]
+col_means4 = colMeans(mean_vid_df4)
 
-vid_itr6 = vid_ldf[vid_index[4]][[1]][, c(2, 17, 18, 20)]
-vid_itr6 = melt(vid_itr6, id.vars = c('title'))
-colnames(vid_itr6)[2] <- "Device"
-colnames(vid_itr6)[3] <- "Percentage_Watch"
+mean_vid_df5 = vid_ldf[vid_index[3]][[1]][, c(17, 18, 20)]
+col_means5 = colMeans(mean_vid_df5)
 
-vid_itr7 = vid_ldf[vid_index[5]][[1]][, c(2, 17, 18, 20)]
-vid_itr7 = melt(vid_itr7, id.vars = c('title'))
-colnames(vid_itr7)[2] <- "Device"
-colnames(vid_itr7)[3] <- "Percentage_Watch"
+mean_vid_df6 = vid_ldf[vid_index[4]][[1]][, c(17, 18, 20)]
+col_means6 = colMeans(mean_vid_df6)
 
-png(file = 'C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/graphs/2.png', width = 1920, height = 1920)
-plt1 = ggplot(vid_itr3, aes(x = title, y = Percentage_Watch, fill = Device))+
+mean_vid_df7 = vid_ldf[vid_index[5]][[1]][, c(17, 18, 20)]
+col_means7 = colMeans(mean_vid_df7)
+
+# Dataframe of means of each device in columns and iterations in rows...
+mean_df = rbind(col_means3, col_means4, col_means5, col_means6, col_means7)
+
+# Converting the wide data to long data...
+mean_df = melt(mean_df)
+colnames(mean_df) = c('Iteration', 'Device', 'Watch_Time')
+
+# saving the plot to the graphs/ directory...
+png(file = 'C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/graphs/2.png', width = 1920, height = 1080)
+ggplot(mean_df, aes(x = Iteration, y = Watch_Time, fill = Device))+
   geom_bar(stat='identity', position = position_dodge())+
   theme(axis.text.x = element_text(angle=90))
-
-plt2 = ggplot(vid_itr4, aes(x = title, y = Percentage_Watch, fill = Device))+
-  geom_bar(stat='identity', position = position_dodge())+
-  theme(axis.text.x = element_text(angle=90)) 
-
-plt3 = ggplot(vid_itr5, aes(x = title, y = Percentage_Watch, fill = Device))+
-  geom_bar(stat='identity', position = position_dodge())+
-  theme(axis.text.x = element_text(angle=90))
-
-plt4 = ggplot(vid_itr6, aes(x = title, y = Percentage_Watch, fill = Device))+
-  geom_bar(stat='identity', position = position_dodge())+
-  theme(axis.text.x = element_text(angle=90))
-
-plt5 = ggplot(vid_itr7, aes(x = title, y = Percentage_Watch, fill = Device))+
-  geom_bar(stat='identity', position = position_dodge())+
-  theme(axis.text.x = element_text(angle=90))
-
-grid.arrange(plt1, plt2, plt3, plt4, plt5)
 dev.off()
