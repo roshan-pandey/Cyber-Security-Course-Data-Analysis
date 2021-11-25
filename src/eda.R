2c40b769e0ecd2f10edc55bae41f5b5d8984fc90
roshan-pandey <roshanpandey330@gmail.com>
2021-11-25 10:32:22 +0000

    Added plot for region from where more learners are enrolling

--- src/eda.R
+++ src/eda.R
@@ -1,15 +1,27 @@
-library('ProjectTemplate')
-load.project()
+# library('ProjectTemplate')
+# load.project()
 
 # Selecting video stats data from all the iterations of the course...
 # video_df = get(project.info$cache[7])
 
-video_df = video
+# People from which region...
+colnames(alpha2lat)[2] = "detected_country"
+with_coordinates = merge(x=enrolments,y=alpha2lat[,c(2,5,6)],by="detected_country", all.x=TRUE)
+colnames(with_coordinates)[14] = "lat"
+colnames(with_coordinates)[15] = "long"
+world <- map_data("world")
+without_na = with_coordinates[!is.na(with_coordinates$long),]
+png(file = 'C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/graphs/3.png', width = 1920, height = 1080)
+ggplot() +
+  geom_map(data = world, map = world, aes(long, lat, map_id = region), color = "black", fill = "lightgray")+
+  stat_density2d(aes(x = long, y = lat, fill = ..level.., alpha = 0.25), bins = 30, data = without_na, geom = "polygon")
+dev.off()
+#######################################################################################################################################
 
 # Finding which device is being use more often to access the course content...
-desktop_percentage = video_df %>% group_by(title) %>% summarize(desktop = mean(desktop_device_percentage))
-mobile_percentage = video_df %>% group_by(title) %>% summarize(mobile = mean(mobile_device_percentage))
-tablet_percentage = video_df %>% group_by(title) %>% summarize(tablet = mean(tablet_device_percentage))
+desktop_percentage = video %>% group_by(title) %>% summarize(desktop = mean(desktop_device_percentage))
+mobile_percentage = video %>% group_by(title) %>% summarize(mobile = mean(mobile_device_percentage))
+tablet_percentage = video %>% group_by(title) %>% summarize(tablet = mean(tablet_device_percentage))
 
 # Creating a dataframe of topic wise device used...
 device_df = cbind(desktop_percentage, mobile_percentage[,2], tablet_percentage[,2])
@@ -25,16 +37,10 @@ ggplot(device_df, aes(x = title, y = Percentage_Watch, fill = Device))+
   theme(axis.text.x = element_text(angle=90))
 dev.off()
 
-# Extracting all the files with video stats...
-#vid_filenames <- list.files("C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/raw_data", pattern="*.csv", full.names=TRUE)
-# vid_ldf <- lapply(vid_filenames, read.csv)
-# vid_index = grep("video_duration", vid_ldf)
-
-
+########################################################################################################################################
 
 # Calculating mean of watch percentage of each device for each iteration of the course...
 
-# mean_vid_df3 = vid_ldf[vid_index[1]][[1]][, c(17, 18, 20)]  
 mean_vid_df3 = cyber.security.3_video.stats[, c(17, 18, 20)]
 iteration_3 = colMeans(mean_vid_df3)
 
@@ -52,14 +58,22 @@ iteration_7 = colMeans(mean_vid_df7)
 
 # Dataframe of means of each device in columns and iterations in rows...
 mean_df = rbind(iteration_3, iteration_4, iteration_5, iteration_6, iteration_7)
+# # mean_df = scale(mean_df)
+# mean_df = cbind(mean_df, c(3, 4, 5, 6, 7))
+# ggplot(as.data.frame(mean_df), aes(x = V4))+
+#   geom_line(aes(y = desktop_device_percentage), col = "red")+
+#   geom_line(aes(y = mobile_device_percentage), col = "blue")+
+#   geom_line(aes(y = tablet_device_percentage), col = "green")
+
 
 # Converting the wide data to long data...
-mean_df = melt(mean_df)
-colnames(mean_df) = c('Iteration', 'Device', 'Watch_Time')
+long_mean_df = melt(mean_df)
+colnames(long_mean_df) = c('Iteration', 'Device', 'Watch_Time')
 
 # saving the plot to the graphs/ directory...
 png(file = 'C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/graphs/2.png', width = 1920, height = 1080)
-ggplot(mean_df, aes(x = Iteration, y = Watch_Time, fill = Device))+
-  geom_bar(stat='identity', position = position_dodge())+
-  theme(axis.text.x = element_text(angle=90))
+ggplot()+
+  geom_bar(long_mean_df, mapping = aes(x = Iteration, y = Watch_Time, fill = Device), stat='identity', position = position_dodge())+
+  geom_line(as.data.frame(mean_df), mapping = aes(x = as.factor(rownames(mean_df)), y = mobile_device_percentage, group = 1))+
+  ylab("Watch Time Percent")
 dev.off()
