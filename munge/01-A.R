d424602b6172afddd1938962dcf17ddb52d1f2c5
roshan-pandey <roshanpandey330@gmail.com>
2021-12-02 07:40:09 +0000

    Code refatoring

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,40 +1,181 @@
-# # Reading all the files and merging similar files...
-# filenames <- list.files("C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/raw_data", pattern="*.csv", full.names=TRUE)
-# 
-# # Reading all the csv files... 
-# ldf <- lapply(filenames, read.csv)
-# 
-# archetype_index = grep("archetype", ldf)
-# archetype_df = rbind(data.frame(ldf[1]), data.frame(ldf[7]), data.frame(ldf[14]), data.frame(ldf[22]), data.frame(ldf[30]), data.frame(ldf[38]), data.frame(ldf[46]))
-# 
-# enrolments_index = grep("enrol", ldf)
-# enrolments_df = rbind(data.frame(ldf[2]), data.frame(ldf[8]), data.frame(ldf[15]), data.frame(ldf[23]), data.frame(ldf[31]), data.frame(ldf[39]), data.frame(ldf[47]))
-# 
-# leaving_index = grep("leaving", ldf)
-# leaving_df = rbind(data.frame(ldf[3]), data.frame(ldf[9]), data.frame(ldf[16]), data.frame(ldf[24]), data.frame(ldf[32]), data.frame(ldf[40]), data.frame(ldf[48]))
-# 
-# question_index = grep("question", ldf)
-# question_df = rbind(data.frame(ldf[4]), data.frame(ldf[10]), data.frame(ldf[17]), data.frame(ldf[25]), data.frame(ldf[33]), data.frame(ldf[41]), data.frame(ldf[49]))
-# 
-# step_index = grep("first_visited_at", ldf)
-# step_df = rbind(data.frame(ldf[5]), data.frame(ldf[11]), data.frame(ldf[18]), data.frame(ldf[26]), data.frame(ldf[34]), data.frame(ldf[42]), data.frame(ldf[50]))
-# 
-# video_index = grep("video_duration", ldf)
-# video_df = rbind(data.frame(ldf[20]), data.frame(ldf[28]), data.frame(ldf[36]), data.frame(ldf[44]), data.frame(ldf[52]))
-# 
-# weekly_index = grep("experience_rating", ldf)
-# weekly_df = rbind(data.frame(ldf[6]), data.frame(ldf[13]), data.frame(ldf[21]), data.frame(ldf[29]), data.frame(ldf[37]), data.frame(ldf[45]), data.frame(ldf[53]))
-# 
-# team_index = grep("team", ldf)
-# team_df = rbind(data.frame(ldf[12]), data.frame(ldf[19]), data.frame(ldf[27]), data.frame(ldf[35]), data.frame(ldf[43]), data.frame(ldf[51]))
-# 
-# 
-# # writing the data frame to csv files...
-# write.csv(archetype_df,"C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/archetype.csv", row.names = FALSE)
-# write.csv(enrolments_df,"C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/enrolments.csv", row.names = FALSE)
-# write.csv(leaving_df,"C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/leaving.csv", row.names = FALSE)
-# write.csv(question_df,"C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/question.csv", row.names = FALSE)
-# write.csv(step_df,"C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/step.csv", row.names = FALSE)
-# write.csv(weekly_df,"C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/weekly.csv", row.names = FALSE)
-# write.csv(video_df,"C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/video.csv", row.names = FALSE)
-# write.csv(team_df,"C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/team.csv", row.names = FALSE)
+# Merging enrollment data from all iterations... (row bind)
+enrolment_i1 = cyber.security.1_enrolments
+enrolment_i2 = cyber.security.2_enrolments
+enrolment_i3 = cyber.security.3_enrolments
+enrolment_i4 = cyber.security.4_enrolments
+enrolment_i5 = cyber.security.5_enrolments
+enrolment_i6 = cyber.security.6_enrolments
+enrolment_i7 = cyber.security.7_enrolments
+enrolments = rbind(enrolment_i1, enrolment_i2, enrolment_i3, enrolment_i4, enrolment_i5, enrolment_i6, enrolment_i7)
+
+# Merging video stats data from all iterations... (row bind)
+video_i3 = cyber.security.3_video.stats
+video_i4 = cyber.security.4_video.stats
+video_i5 = cyber.security.5_video.stats
+video_i6 = cyber.security.6_video.stats
+video_i7 = cyber.security.7_video.stats
+video = rbind(video_i3, video_i4, video_i5, video_i6, video_i7)
+
+# Merging leaving survey response data from all iterations... (row bind)
+leaving_i4 = cyber.security.4_leaving.survey.responses
+leaving_i5 = cyber.security.5_leaving.survey.responses
+leaving_i6 = cyber.security.6_leaving.survey.responses
+leaving_i7 = cyber.security.7_leaving.survey.responses
+leaving = rbind(leaving_i4, leaving_i5, leaving_i6, leaving_i7)
+
+# Merging weekly sentiment data from all iterations... (row bind)
+weekly_i5 = cyber.security.5_weekly.sentiment.survey.responses
+weekly_i6 = cyber.security.6_weekly.sentiment.survey.responses
+weekly_i7 = cyber.security.7_weekly.sentiment.survey.responses
+weekly = rbind(weekly_i5, weekly_i6, weekly_i7)
+
+
+# Calculating number of enrollments each year...
+# dim() will give number of rows and cols. Number of rows represent number of learners enrolled...
+batch_1 = dim(enrolment_i1)
+batch_2 = dim(enrolment_i2)
+batch_3 = dim(enrolment_i3)
+batch_4 = dim(enrolment_i4)
+batch_5 = dim(enrolment_i5)
+batch_6 = dim(enrolment_i6)
+batch_7 = dim(enrolment_i7)
+
+# Creating Data frame of number of rows and cols of enrollment dataset from each iterations...
+dim_df = as.data.frame(rbind(batch_1, batch_2, batch_3, batch_4, batch_5, batch_6, batch_7)) 
+colnames(dim_df) = c("Number_of_learners_enrolled", "Number_of_features") # changing col names...
+dim_df$batch = rownames(dim_df) # Creating new column to hold row names...
+row.names(dim_df) <- NULL # making rows as NULL...
+
+
+colnames(alpha2lat)[2] = "detected_country"
+alpha2lat$detected_country = trimws(alpha2lat$detected_country)
+with_coordinates = merge(x = enrolments, y = alpha2lat[,c(2,5,6)], by = "detected_country", all.x = TRUE)
+colnames(with_coordinates)[14] = "lat"
+colnames(with_coordinates)[15] = "long"
+without_na = with_coordinates[!is.na(with_coordinates$long),]
+
+
+
+
+# Reference: https://stackoverflow.com/questions/28078431/how-to-create-a-world-heat-map-in-r-on-continent-level
+world = map_data("world")
+url = "https://gist.githubusercontent.com/hrbrmstr/91ea5cc9474286c72838/raw/f3fde312c9b816dff3994f39f2bcda03209eff8f/continents.json"
+stop_for_status(GET(url, write_disk("C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/continents.json", overwrite = TRUE)))
+continents = readOGR("C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/continents.json")
+continents_map = fortify(continents, region="CONTINENT")
+
+cont_data = read.table(text="id,  value
+Europe,  59.185692
+Australia, 4.355385
+Asia,  15.287538
+North America, 10.400154
+South America, 2.140462
+Africa,  7.450000
+Antarctica, 0", header=TRUE, stringsAsFactors=FALSE, sep = ",")
+
+
+
+colnames(alpha2lat)[2] = "detected_country"
+alpha2lat$detected_country = trimws(alpha2lat$detected_country)
+with_coordinates = merge(x = enrolments, y = alpha2lat[,c(2,5,6)], by = "detected_country", all.x = TRUE)
+colnames(with_coordinates)[14] = "lat"
+colnames(with_coordinates)[15] = "long"
+without_na = with_coordinates[!is.na(with_coordinates$long),]
+
+
+
+gender = data.frame(gender = enrolments$gender[enrolments$gender != 'Unknown'])
+gender = as.data.frame(table(gender$gender))
+
+edu_lvl = data.frame(edu_lvl = enrolments$highest_education_level[enrolments$highest_education_level != 'Unknown'])
+edu_lvl = as.data.frame(table(edu_lvl$edu_lvl))
+
+emp_status = data.frame(emp_status = enrolments$employment_status[enrolments$employment_status != 'Unknown'])
+emp_status = as.data.frame(table(emp_status$emp_status))
+
+emp_area = data.frame(emp_area = enrolments$employment_area[enrolments$employment_area != 'Unknown'])
+emp_area = as.data.frame(table(emp_area$emp_area))
+
+age_range = data.frame(age_range = enrolments$age_range[enrolments$age_range != 'Unknown'])
+age_range = as.data.frame(table(age_range$age_range))
+
+
+
+# Finding which device is being use more often to access the course content...
+desktop_percentage = video %>% group_by(title) %>% summarize(desktop = mean(desktop_device_percentage))
+mobile_percentage = video %>% group_by(title) %>% summarize(mobile = mean(mobile_device_percentage))
+tablet_percentage = video %>% group_by(title) %>% summarize(tablet = mean(tablet_device_percentage))
+
+# Creating a dataframe of topic wise device used...
+device_df = cbind(desktop_percentage, mobile_percentage[,2], tablet_percentage[,2])
+device_df = as.data.frame(colMeans(device_df[2:4]))
+colnames(device_df) = c("Usage")
+
+
+
+mean_vid_df3 = cyber.security.3_video.stats[, c(17, 18, 20)]
+iteration_3 = colMeans(mean_vid_df3)
+
+mean_vid_df4 = cyber.security.4_video.stats[, c(17, 18, 20)]
+iteration_4 = colMeans(mean_vid_df4)
+
+mean_vid_df5 = cyber.security.5_video.stats[, c(17, 18, 20)]
+iteration_5 = colMeans(mean_vid_df5)
+
+mean_vid_df6 = cyber.security.6_video.stats[, c(17, 18, 20)]
+iteration_6 = colMeans(mean_vid_df6)
+
+mean_vid_df7 = cyber.security.7_video.stats[, c(17, 18, 20)]
+iteration_7 = colMeans(mean_vid_df7)
+
+# Dataframe of means of each device in columns and iterations in rows...
+mean_df = rbind(iteration_3, iteration_4, iteration_5, iteration_6, iteration_7)
+colnames(mean_df) = c("Desktop", "Mobile", "Tablet")
+# Converting the wide data to long data...
+long_mean_df = melt(mean_df)
+colnames(long_mean_df) = c('Iteration', 'Device', 'Watch_Time')
+
+
+
+cleaned_string = string_cleaner(weekly$reason)
+clean_txt_df = data.frame(txt = strsplit(cleaned_string, split = " "))
+clean_txt_df = data.frame(table(clean_txt_df))
+colnames(clean_txt_df) = c("txt", "freq")
+
+sent <- get_nrc_sentiment(as.character(clean_txt_df$txt))
+sent_score = colSums(sent)
+sent_score_df = rbind(colnames(sent), sent_score)
+sent_score_df = as.data.frame(t(sent_score_df))
+sent_score_df = subset(sent_score_df, select = -c(1))
+sent_score_df$sent_score = as.numeric(sent_score_df$sent_score)
+
+
+
+leaving_reason = data.frame(table(leaving$leaving_reason))
+colnames(leaving_reason) = c('reason', 'freq')
+leaving_reason$reason = gsub("[^[:alnum:]]", " ",as.character(leaving_reason$reason))
+leaving_reason$reason = gsub("â", "",as.character(leaving_reason$reason))
+leaving_reason$reason = gsub("n t", "n't",as.character(leaving_reason$reason))
+
+hsize = 1
+
+leaving_reason = leaving_reason %>% 
+  mutate(x = hsize)
+
+
+
+total_views = (video %>% group_by(title) %>% summarize(total_views = mean(total_views)))
+total_views$full_watch = (video %>% group_by(title) %>% summarize(watch_100 = mean(viewed_onehundred_percent)))$watch_100
+total_views$qua3_watch = (video %>% group_by(title) %>% summarize(watch_75 = mean(viewed_seventyfive_percent)))$watch_75
+total_views$half_watch = (video %>% group_by(title) %>% summarize(watch_50 = mean(viewed_fifty_percent)))$watch_50
+total_views$qua_watch = (video %>% group_by(title) %>% summarize(watch_25 = mean(viewed_twentyfive_percent)))$watch_25
+total_views = subset(total_views, select = -c(title))
+total_views = normalize(total_views, method = "range", range = c(0,1))
+total_views = as.data.frame(cbind(total_views, video$video_duration[1:13]))
+colnames(total_views)[6] = "Video_Length"
+colnames(total_views)[1] = "View_Count"
+
+total_views$Video_Length = as.factor(total_views$Video_Length)
+views = subset(total_views, select = -c(View_Count))
+views = melt(views, id.vars = c('Video_Length'))
+colnames(views) = c("Video_Length", "Watch_Status", "Value")
--- src/eda.R
+++ src/eda.R
@@ -1,55 +1,15 @@
-world <- map_data("world")
-options(repr.plot.width = 1920, repr.plot.height = 1080) 
-######################################################################################################################################
-enrolment_i1 = cyber.security.1_enrolments
-enrolment_i2 = cyber.security.2_enrolments
-enrolment_i3 = cyber.security.3_enrolments
-enrolment_i4 = cyber.security.4_enrolments
-enrolment_i5 = cyber.security.5_enrolments
-enrolment_i6 = cyber.security.6_enrolments
-enrolment_i7 = cyber.security.7_enrolments
 
-batch_1 = dim(enrolment_i1)
-batch_2 = dim(enrolment_i2)
-batch_3 = dim(enrolment_i3)
-batch_4 = dim(enrolment_i4)
-batch_5 = dim(enrolment_i5)
-batch_6 = dim(enrolment_i6)
-batch_7 = dim(enrolment_i7)
 
-dim_df = as.data.frame(rbind(batch_1, batch_2, batch_3, batch_4, batch_5, batch_6, batch_7))
-colnames(dim_df) = c("Number_of_learners_enrolled", "Number_of_features")
-dim_df$batch = rownames(dim_df)
-row.names(dim_df) <- NULL
-
+# Plot for Number of learners enrolled over different iteration...
 performance = ggplot()+
   geom_line(data = dim_df, mapping = aes(x = batch, y = Number_of_learners_enrolled, group = 1), col = "blue")+
   geom_point(data = dim_df, mapping = aes(x = batch, y = Number_of_learners_enrolled, group = 1), col = "red", size = 2)+
   xlab("Batches")+
-  ylab("Number of Learners")+
-  theme(axis.text.x = element_text(angle=90))
+  ylab("Number of Learners")
 
-png(file = './graphs/1.png', width = 1920, height = 1080)
-grid.arrange(performance)
-dev.off()
 #######################################################################################################################################
 
-# Reference: https://stackoverflow.com/questions/28078431/how-to-create-a-world-heat-map-in-r-on-continent-level
-
-url = "https://gist.githubusercontent.com/hrbrmstr/91ea5cc9474286c72838/raw/f3fde312c9b816dff3994f39f2bcda03209eff8f/continents.json"
-stop_for_status(GET(url, write_disk("C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/continents.json", overwrite = TRUE)))
-continents = readOGR("C:/DataScience/R/CSC8631_Data_Management/Cyber-Security-Data-Analysis/data/continents.json")
-continents_map = fortify(continents, region="CONTINENT")
-
-cont_data = read.table(text="id,  value
-Europe,  59.185692
-Australia, 4.355385
-Asia,  15.287538
-North America, 10.400154
-South America, 2.140462
-Africa,  7.450000
-Antarctica, 0", header=TRUE, stringsAsFactors=FALSE, sep = ",")
-
+# Plot for Continent from where most learners are enrolling...
 continents_plot = ggplot()+
   geom_map(data = continents_map, map = continents_map, aes(x=long, y = lat, map_id = id), color = "gray")+
   geom_map(data = cont_data, map = continents_map, aes(map_id = id, fill = value), color = "gray")+
@@ -57,49 +17,22 @@ continents_plot = ggplot()+
   xlab("Longitude")+
   ylab("Latitude")+
   coord_equal()+
-  labs(x=NULL, y=NULL)
+  labs(x=NULL, y=NULL)+
+  theme(text = element_text(size = 20))
 
-png(file = './graphs/2.png', width = 1920, height = 1080)
-grid.arrange(continents_plot)
-dev.off()
 #######################################################################################################################################
-# People from which region...
 
-colnames(alpha2lat)[2] = "detected_country"
-alpha2lat$detected_country = trimws(alpha2lat$detected_country)
-with_coordinates = merge(x = enrolments, y = alpha2lat[,c(2,5,6)], by = "detected_country", all.x = TRUE)
-colnames(with_coordinates)[14] = "lat"
-colnames(with_coordinates)[15] = "long"
-without_na = with_coordinates[!is.na(with_coordinates$long),]
-
+# Plot for Country from where most learners are enrolling...
 country_plot = ggplot() +
   geom_map(data = world, map = world, aes(long, lat, map_id = region), color = "black", fill = "lightgray")+
   stat_density2d(aes(x = long, y = lat, fill = ..level.., alpha = 0.5), bins = 30, data = without_na, geom = "polygon")+
   xlab("Longitude")+
-  ylab("Latitude")
-
-png(file = './graphs/3.png', width = 1920, height = 1080)
-grid.arrange(country_plot)
-dev.off()
+  ylab("Latitude")+
+  theme(text = element_text(size = 20))
 
 #######################################################################################################################################
 
-# Which type of people are joining the course most?
-gender = data.frame(gender = enrolments$gender[enrolments$gender != 'Unknown'])
-gender = as.data.frame(table(gender$gender))
-
-edu_lvl = data.frame(edu_lvl = enrolments$highest_education_level[enrolments$highest_education_level != 'Unknown'])
-edu_lvl = as.data.frame(table(edu_lvl$edu_lvl))
-
-emp_status = data.frame(emp_status = enrolments$employment_status[enrolments$employment_status != 'Unknown'])
-emp_status = as.data.frame(table(emp_status$emp_status))
-
-emp_area = data.frame(emp_area = enrolments$employment_area[enrolments$employment_area != 'Unknown'])
-emp_area = as.data.frame(table(emp_area$emp_area))
-
-age_range = data.frame(age_range = enrolments$age_range[enrolments$age_range != 'Unknown'])
-age_range = as.data.frame(table(age_range$age_range))
-
+# Plot for Which type of people are joining the course most..?
 gg_gender = ggplot(data = gender, aes(x = Var1, y = Freq, fill = Var1))+
   geom_bar(stat = "identity")+
   labs(fill = "Gender")+
@@ -138,61 +71,18 @@ lay = rbind(c(1,2),
             c(5,5))
 arrange_plot = grid.arrange(gg_gender, gg_age_range, gg_emp_status, gg_edu_lvl, gg_emp_area, layout_matrix = lay)
 
-
-png(file = './graphs/4.png', width = 1920, height = 1080)
-grid.arrange(gg_gender, gg_age_range, gg_emp_status, gg_edu_lvl, gg_emp_area, layout_matrix = lay)
-dev.off()
 #######################################################################################################################################
 
-# Finding which device is being use more often to access the course content...
-desktop_percentage = video %>% group_by(title) %>% summarize(desktop = mean(desktop_device_percentage))
-mobile_percentage = video %>% group_by(title) %>% summarize(mobile = mean(mobile_device_percentage))
-tablet_percentage = video %>% group_by(title) %>% summarize(tablet = mean(tablet_device_percentage))
-
-# Creating a dataframe of topic wise device used...
-device_df = cbind(desktop_percentage, mobile_percentage[,2], tablet_percentage[,2])
-device_df = as.data.frame(colMeans(device_df[2:4]))
-colnames(device_df) = c("Usage")
-
+# Plot for Most widely used device to access the content...
 device_plot = ggplot(device_df, aes(x = row.names(device_df), y = Usage, fill = row.names(device_df)))+ 
   geom_bar(stat = "identity")+
   xlab("Device")+
   labs(fill = "Devices")+
-  theme(text = element_text(size = 25))+
   scale_fill_brewer(palette = "Set3")
-
-
-# saving the plot to the graphs/ directory...
-png(file = './graphs/5.png', width = 1920, height = 1080)
-grid.arrange(device_plot)
-dev.off()
 
 ########################################################################################################################################
 
-# Calculating mean of watch percentage of each device for each iteration of the course...
-
-mean_vid_df3 = cyber.security.3_video.stats[, c(17, 18, 20)]
-iteration_3 = colMeans(mean_vid_df3)
-
-mean_vid_df4 = cyber.security.4_video.stats[, c(17, 18, 20)]
-iteration_4 = colMeans(mean_vid_df4)
-
-mean_vid_df5 = cyber.security.5_video.stats[, c(17, 18, 20)]
-iteration_5 = colMeans(mean_vid_df5)
-
-mean_vid_df6 = cyber.security.6_video.stats[, c(17, 18, 20)]
-iteration_6 = colMeans(mean_vid_df6)
- 
-mean_vid_df7 = cyber.security.7_video.stats[, c(17, 18, 20)]
-iteration_7 = colMeans(mean_vid_df7)
-
-# Dataframe of means of each device in columns and iterations in rows...
-mean_df = rbind(iteration_3, iteration_4, iteration_5, iteration_6, iteration_7)
-colnames(mean_df) = c("Desktop", "Mobile", "Tablet")
-# Converting the wide data to long data...
-long_mean_df = melt(mean_df)
-colnames(long_mean_df) = c('Iteration', 'Device', 'Watch_Time')
-
+# Most widely used device to access the content in each iteration..?
 device_ittr_plot = ggplot()+
   geom_bar(long_mean_df, mapping = aes(x = Iteration, y = Watch_Time, fill = Device), stat = 'identity', position = position_dodge())+
   geom_line(as.data.frame(mean_df), mapping = aes(x = as.factor(rownames(mean_df)), y = Mobile, group = 1), size = 1.5, color = "DarkBlue")+
@@ -201,36 +91,15 @@ device_ittr_plot = ggplot()+
   xlab("Batch Iteration")+
   scale_fill_brewer(palette = "Set3")
 
-
-# saving the plot to the graphs/ directory...
-png(file = './graphs/6.png', width = 1920, height = 1080)
-grid.arrange(device_ittr_plot)
-dev.off()
-
 #######################################################################################################################################
 
-cleaned_string = string_cleaner(weekly$reason)
-clean_txt_df = data.frame(txt = strsplit(cleaned_string, split = " "))
-clean_txt_df = data.frame(table(clean_txt_df))
-colnames(clean_txt_df) = c("txt", "freq")
-
-sent <- get_nrc_sentiment(as.character(clean_txt_df$txt))
-sent_score = colSums(sent)
-sent_score_df = rbind(colnames(sent), sent_score)
-sent_score_df = as.data.frame(t(sent_score_df))
-sent_score_df = subset(sent_score_df, select = -c(1))
-sent_score_df$sent_score = as.numeric(sent_score_df$sent_score)
-
+# Word Cloud for Overall sentiments of learners...
 sentiments_word_cloud = ggplot(clean_txt_df, aes(label = txt, size = freq, color = factor(sample.int(10, nrow(clean_txt_df), replace = TRUE)),)) +
   geom_text_wordcloud() +
   scale_size_area(max_size = 35) +
   theme_minimal()
 
-png(file = './graphs/7.png', width = 1920, height = 1080)
-grid.arrange(sentiments_word_cloud)
-dev.off()
-
-
+# Plot for Overall sentiment scores of learners...
 sent_plot = ggplot(data = sent_score_df, mapping = aes(x = row.names(sent_score_df), y = sent_score, fill = row.names(sent_score_df)))+
   geom_bar(stat = "identity")+
   xlab("Sentiments")+
@@ -238,26 +107,9 @@ sent_plot = ggplot(data = sent_score_df, mapping = aes(x = row.names(sent_score_
   labs(fill = "Sentiments")+
   scale_fill_brewer(palette = "Set3")
 
-
-
-png(file = './graphs/8.png', width = 1920, height = 1080)
-grid.arrange(sent_plot)
-dev.off()
-
 #######################################################################################################################################
-leaving_reason = data.frame(table(leaving$leaving_reason))
-colnames(leaving_reason) = c('reason', 'freq')
-leaving_reason$reason = gsub("[^[:alnum:]]", " ",as.character(leaving_reason$reason))
-leaving_reason$reason = gsub("â", "",as.character(leaving_reason$reason))
-leaving_reason$reason = gsub("n t", "n't",as.character(leaving_reason$reason))
 
-
-
-hsize = 1
-
-leaving_reason = leaving_reason %>% 
-  mutate(x = hsize)
-
+# Plot for Reason for leaving the course...
 leaving_reason_plot = ggplot(leaving_reason, aes(x = hsize, y = freq, fill = reason))+
   geom_col()+
   coord_polar(theta = "y")+
@@ -269,28 +121,9 @@ leaving_reason_plot = ggplot(leaving_reason, aes(x = hsize, y = freq, fill = rea
   geom_text(aes(label = freq), position = position_stack(vjust = 0.5)) +
   theme(axis.text=element_blank(), axis.text.x=element_blank(), axis.text.y=element_blank())
 
-png(file = './graphs/9.png', width = 1920, height = 1080)
-grid.arrange(leaving_reason_plot)
-dev.off()
-
 #######################################################################################################################################
 
-total_views = (video %>% group_by(title) %>% summarize(total_views = mean(total_views)))
-total_views$full_watch = (video %>% group_by(title) %>% summarize(watch_100 = mean(viewed_onehundred_percent)))$watch_100
-total_views$qua3_watch = (video %>% group_by(title) %>% summarize(watch_75 = mean(viewed_seventyfive_percent)))$watch_75
-total_views$half_watch = (video %>% group_by(title) %>% summarize(watch_50 = mean(viewed_fifty_percent)))$watch_50
-total_views$qua_watch = (video %>% group_by(title) %>% summarize(watch_25 = mean(viewed_twentyfive_percent)))$watch_25
-total_views = subset(total_views, select = -c(title))
-total_views = normalize(total_views, method = "range", range = c(0,1))
-total_views = as.data.frame(cbind(total_views, video$video_duration[1:13]))
-colnames(total_views)[6] = "Video_Length"
-colnames(total_views)[1] = "View_Count"
-
-total_views$Video_Length = as.factor(total_views$Video_Length)
-views = subset(total_views, select = -c(View_Count))
-views = melt(views, id.vars = c('Video_Length'))
-colnames(views) = c("Video_Length", "Watch_Status", "Value")
-
+# Plot for Optimal Video Length...
 views_comparison_plot = ggplot(views, aes(x = Video_Length, y = Value, group = Watch_Status, color = Watch_Status))+
   geom_bar(data = total_views, mapping = aes(x = Video_Length, y = View_Count), 
            fill = "steelblue", stat = "identity", inherit.aes = FALSE)+
@@ -299,7 +132,3 @@ views_comparison_plot = ggplot(views, aes(x = Video_Length, y = Value, group = W
   xlab("Video Length")+
   ylab("Normalized Watch Percent")+
   labs(fill = "Watch Status")
-
-png(file = './graphs/10.png', width = 1920, height = 1080)
-grid.arrange(views_comparison_plot)
-dev.off()
